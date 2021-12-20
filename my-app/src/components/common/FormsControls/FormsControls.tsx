7e75d39055ccc9adc7caa179597bdd44b4de6ec8
Alex <alexandepp@gmail.com>
2021-12-20 12:31:24 +0300

    samurai 77 finish

--- my-app/src/components/common/FormsControls/FormsControls.tsx
+++ my-app/src/components/common/FormsControls/FormsControls.tsx
@@ -1,35 +1,83 @@
 import React from "react";
-import {WrappedFieldProps} from "redux-form";
+import {WrappedFieldMetaProps, WrappedFieldProps} from "redux-form";
 import styles from './FormsControls.module.css'
 
-export const Textarea: React.FC<WrappedFieldProps> =
-    ({input, meta, ...props}) => {
-
-    const hasError = meta.touched && meta.error;
-    return (
-        <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
-        {/*<div className={styles.error}>*/}
-            <div>
-                <textarea{...input} {...props} />
-            </div>
-            { hasError && <span>{meta.error}</span> }
-        </div>
-    )
+type FormControlPropsType = {
+    meta: WrappedFieldMetaProps
 }
 
-export const Input: React.FC<WrappedFieldProps> =
-    ({input, meta, ...props}) => {
-        const hasError = meta.touched && meta.error;
+const FormControl: React.FC<FormControlPropsType> =
+    ({meta: {touched, error}, children}) => {
+        const hasError = touched && error;
         return (
             <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
                 {/*<div className={styles.error}>*/}
                 <div>
-                    <input {...input} {...props} />
+                    {children}
                 </div>
-                { hasError && <span>{meta.error}</span> }
+                { hasError && <span>{error}</span> }
             </div>
         )
-    }
+}
+
+export const Textarea: React.FC<WrappedFieldProps> = (props) => {
+    const {input, meta, ...restProps} = props;
+    return <FormControl {...props}><textarea{...input}{...restProps}/></FormControl>
+}
+
+export const Input: React.FC<WrappedFieldProps> = (props) => {
+    // let input = props.input
+    // let meta = props.meta
+    // const hasError = meta.touched && meta.error
+    const {input, meta, ...restProps} = props
+    return <FormControl {...props}><input{...input}{...restProps}/></FormControl>
+}
+
+// const FormControl: React.FC<FormControlPropsType> =
+//     ({meta: {touched, error}, children}) => {
+//         const hasError = touched && error;
+//         return (
+//             <div className={styles.formControl + '' + (hasError ? styles.error : "")}>
+//                 <div>
+//                     {children}
+//                 </div>
+//                 {hasError && <span>{error}</span>}
+//             </div>
+//
+//         )
+//     }
+
+// export const Textarea = (props: any) => {
+//     return <FormControl {...props}><textarea {...input} {...props}/></FormControl>
+// }
+
+// export const Textarea: React.FC<WrappedFieldProps> =
+//     ({input, meta, ...props}) => {
+//     const hasError = meta.touched && meta.error;
+//     return (
+//         <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
+//         {/*<div className={styles.error}>*/}
+//             <div>
+//                 <textarea{...input} {...props} />
+//             </div>
+//             { hasError && <span>{meta.error}</span> }
+//         </div>
+//     )
+// }
+
+// export const Input: React.FC<WrappedFieldProps> =
+//     ({input, meta, ...props}) => {
+//         const hasError = meta.touched && meta.error;
+//         return (
+//             <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
+//                 {/*<div className={styles.error}>*/}
+//                 <div>
+//                     <input {...input} {...props} />
+//                 </div>
+//                 { hasError && <span>{meta.error}</span> }
+//             </div>
+//         )
+//     }
 
 
 // export const Textarea: React.FC<WrappedFieldProps> = ({input, meta, ...props}) => {
